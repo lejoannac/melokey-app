e7f32be278586aeef60ab6822a766890a00ba969
lejoannac <jxcai534@gmail.com>
2024-07-24 13:51:57 -0700

    Initial Commit

---
+++ melokey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  melokey
+//
+//  Created by joanna on 2024-07-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ melokey/melokeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  melokeyApp.swift
+//  melokey
+//
+//  Created by joanna on 2024-07-24.
+//
+
+import SwiftUI
+
+@main
+struct melokeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
