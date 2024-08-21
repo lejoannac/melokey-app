598504016bd325d2645c4afe61d0ef0634c96756
lejoannac <jxcai534@gmail.com>
2024-08-21 14:32:40 -0700

    Layout fixed

--- melokey/ContentView.swift
+++ melokey/ContentView.swift
@@ -19,17 +19,57 @@ struct ContentView: View {
     }
     var body: some View {
         ZStack {
-            
-            Text("melokey")
-                .font(Font.custom("March", size: 175))
-                .foregroundColor(Color(red: 0.13, green: 0.20, blue: 0.54))
+            // Background color
+            Color(red: 0.91, green: 0.93, blue: 0.96)
+                .edgesIgnoringSafeArea(.all)
                 .rotationEffect(.degrees(90))
             
-            
+            VStack(spacing: 50) { // Adjust the spacing as needed
+                // Melokey title at the top
+                Text("melokey")
+                    .font(Font.custom("March", size: 175))
+                    .foregroundColor(Color(red: 0.13, green: 0.20, blue: 0.54))
+                    .rotationEffect(.degrees(0)) // No rotation needed
+
+                // Create New Project button
+                VStack(spacing: 10) {
+                    Text("Create New Project")
+                        .font(Font.custom("Gazpacho", size: 36).weight(.black))
+                        .foregroundColor(.black)
+                        .rotationEffect(.degrees(0)) // No rotation needed
+                }
+                .padding(10)
+                .frame(width: 365, height: 65)
+                .background(.white)
+                .cornerRadius(6)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .inset(by: 0.50)
+                        .stroke(.black, lineWidth: 0.50)
+                )
+                
+                // My projects button
+                VStack(spacing: 10) {
+                    Text("My projects")
+                        .font(Font.custom("Gazpacho", size: 36).weight(.black))
+                        .foregroundColor(.black)
+                        .rotationEffect(.degrees(0)) // No rotation needed
+                }
+                .padding(10)
+                .frame(width: 232, height: 65)
+                .background(.white)
+                .cornerRadius(6)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .inset(by: 0.50)
+                        .stroke(.black, lineWidth: 0.50)
+                )
+            }
         }
         .frame(width: 834, height: 1194)
-        .background(Color(red: 0.91, green: 0.93, blue: 0.96))
-        .rotationEffect(Angle(degrees: -90))
+    
+
+
         
         
         
