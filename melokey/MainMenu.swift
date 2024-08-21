28f67c53f5bab43510fe4b03980feb7e8557920f
lejoannac <jxcai534@gmail.com>
2024-08-21 10:09:25 -0700

    added march font and started figuring out how to import some figma texts into xcode

---
+++ melokey/MainMenu.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenu.swift
+//  melokey
+//
+//  Created by joanna on 2024-08-21.
+//
+
+import SwiftUI
+
+struct MainMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMenu()
+}
