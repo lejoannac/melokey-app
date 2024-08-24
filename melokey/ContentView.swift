e07129795dfd595f747c85bcf03401908cb9340a
lejoannac <jxcai534@gmail.com>
2024-08-24 16:12:39 -0700

    setup text and experimenting with the sliders/toggles

--- melokey/ContentView.swift
+++ melokey/ContentView.swift
@@ -1,101 +1,74 @@
-//
-//  ContentView.swift
-//  melokey
-//
-//  Created by joanna on 2024-07-24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    
-//    init() {
-//        for fontFamily in UIFont.familyNames {
-//            for font in UIFont.fontNames(forFamilyName: fontFamily) {
-//                print ("--\(font)")
-//            }
-//        }
-//        
-//    }
     var body: some View {
-        ZStack {
-            
-            Color(red: 0.91, green: 0.93, blue: 0.96)
-                .edgesIgnoringSafeArea(.all)
-                .rotationEffect(.degrees(90))
-            
-            VStack(spacing: 50) {
-
-                Text("melokey")
-                    .font(Font.custom("March", size: 175))
-                    .foregroundColor(Color(red: 0.13, green: 0.20, blue: 0.54))
-                    .rotationEffect(.degrees(0))
-                
-                
-                VStack(spacing: 10) {
-                    Text("Create New Project")
-                        .font(Font.custom("Gazpacho", size: 36).weight(.black))
-                        .foregroundColor(.black)
+        NavigationView {
+            GeometryReader { geometry in
+                ZStack {
+                    Color(red: 0.91, green: 0.93, blue: 0.96)
+                        .edgesIgnoringSafeArea(.all)
                         .rotationEffect(.degrees(0))
                     
-                }
-                .padding(10)
-                .frame(width: 365, height: 65)
-                .background(.white)
-                .cornerRadius(6)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 6)
-                        .inset(by: 0.50)
-                        .stroke(.black, lineWidth: 0.50)
-                )
-                
-                // My projects button
-                VStack(spacing: 10) {
-                    Text("My projects")
-                        .font(Font.custom("Gazpacho", size: 36).weight(.black))
-                        .foregroundColor(.black)
-                        .rotationEffect(.degrees(0))
-                }
-                .padding(10)
-                .frame(width: 232, height: 65)
-                .background(.white)
-                .cornerRadius(6)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 6)
-                        .inset(by: 0.50)
-                        .stroke(.black, lineWidth: 0.50)
-                )
-                
-              
-                                Button(action: {
-                                    print("Settings button tapped")
-                                }) {
-                                    IconSettings(size: "48")
-                                }
-                                .padding(.bottom, 30)                             }
-                            .frame(maxHeight: .infinity)
+                    VStack(spacing: 50) {
+                        Text("melokey")
+                            .font(Font.custom("March", size: 175))
+                            .foregroundColor(Color(red: 0.13, green: 0.20, blue: 0.54))
+                            .rotationEffect(.degrees(0))
+                        
+                        VStack(spacing: 10) {
+                            Text("Create New Project")
+                                .font(Font.custom("Gazpacho", size: 36).weight(.black))
+                                .foregroundColor(.black)
                         }
-                        .frame(width: 834, height: 1194)
+                        .padding(10)
+                        .frame(width: 365, height: 65)
+                        .background(.white)
+                        .cornerRadius(6)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .inset(by: 0.5)
+                                .stroke(.black, lineWidth: 0.5)
+                        )
+                        
+                        VStack(spacing: 10) {
+                            Text("My projects")
+                                .font(Font.custom("Gazpacho", size: 36).weight(.black))
+                                .foregroundColor(.black)
+                        }
+                        .padding(10)
+                        .frame(width: 232, height: 65)
+                        .background(.white)
+                        .cornerRadius(6)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .inset(by: 0.5)
+                                .stroke(.black, lineWidth: 0.5)
+                        )
+                        
+                        NavigationLink(destination: SettingsView()) {
+                            IconSettings(size: "48")
+                        }
+                        .padding(.bottom, 20)
                     }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
+                .frame(width: geometry.size.width, height: geometry.size.height)
+            }
+            .navigationBarHidden(true)
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
 
-                struct IconSettings: View {
-                    var size: String
+struct IconSettings: View {
+    var size: String
 
-                    var body: some View {
-                        Image("Settings")
-                            .resizable()
-                            .frame(width: CGFloat(Double(size)!), height: CGFloat(Double(size)!))
-                            .foregroundColor(.black)
-                    }
-                }
-            
-        
-    
-
-
-struct Constants {
-  static let SchemesOnPrimary: Color = .white
+    var body: some View {
+        Image("Settings")
+            .resizable()
+            .frame(width: CGFloat(Double(size)!), height: CGFloat(Double(size)!))
+            .foregroundColor(.black)
+    }
 }
 
 #Preview {
--- melokey/SettingsView.swift
+++ melokey/SettingsView.swift
@@ -1,18 +1,54 @@
-//
-//  SettingsView.swift
-//  melokey
-//
-//  Created by joanna on 2024-08-21.
-//
-
 import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack(spacing: 20) {
+                Text("settings.")
+                    .font(Font.custom("Gazpacho", size: 64).weight(.black))
+                    .foregroundColor(.black)
+                
+                VStack(spacing: 40) {
+                    Text("Dark Mode")
+                    Text("Sound FX Volume")
+                    Text("Metronome Volume")
+                    Text("Master Volume")
+                }
+                .font(Font.custom("Gazpacho", size: 20).weight(.black))
+                .foregroundColor(.black)
+                
+                HStack(spacing: 20) {
+                    Text("Master Volume")
+                    Slider(value: .constant(0.5))
+                        .frame(width: 200)
+                }
+                .padding()
+                .background(Color(red: 0.91, green: 0.87, blue: 0.97))
+                .cornerRadius(16)
+  =
+                HStack(spacing: 20) {
+                    Text("Dark Mode")
+                    Toggle("", isOn: .constant(true))
+                        .labelsHidden()
+                }
+                .padding()
+                .background(Color(red: 0.90, green: 0.88, blue: 0.91))
+                .cornerRadius(100)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 100)
+                        .stroke(Color(red: 0.47, green: 0.45, blue: 0.49), lineWidth: 1)
+                )
+                .padding(.top, 20)
+            }
+            .padding()
+            .rotationEffect(.degrees(0))
+            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 4, y: 4)
+        }
     }
 }
 
-#Preview {
-    SettingsView()
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
 }
