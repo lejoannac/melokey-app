32cc4f6217b0475620c76511f730ccc6d02d6c93
lejoannac <jxcai534@gmail.com>
2024-08-28 15:22:58 -0700

    troubleshooting the issue where everything shifts down when navigating

--- melokey/ContentView.swift
+++ melokey/ContentView.swift
@@ -3,56 +3,51 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            GeometryReader { geometry in
-                ZStack {
-                    Color(red: 0.91, green: 0.93, blue: 0.96)
-                        .edgesIgnoringSafeArea(.all)
-                        .rotationEffect(.degrees(0))
+            ZStack {
+                Color(red: 0.91, green: 0.93, blue: 0.96)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack(spacing: 50) {
+                    Text("melokey")
+                        .font(Font.custom("March", size: 175))
+                        .foregroundColor(Color(red: 0.13, green: 0.20, blue: 0.54))
                     
-                    VStack(spacing: 50) {
-                        Text("melokey")
-                            .font(Font.custom("March", size: 175))
-                            .foregroundColor(Color(red: 0.13, green: 0.20, blue: 0.54))
-                            .rotationEffect(.degrees(0))
-                        
-                        VStack(spacing: 10) {
-                            Text("Create New Project")
-                                .font(Font.custom("Gazpacho", size: 36).weight(.black))
-                                .foregroundColor(.black)
-                        }
-                        .padding(10)
-                        .frame(width: 365, height: 65)
-                        .background(.white)
-                        .cornerRadius(6)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 6)
-                                .inset(by: 0.5)
-                                .stroke(.black, lineWidth: 0.5)
-                        )
-                        
-                        VStack(spacing: 10) {
-                            Text("My projects")
-                                .font(Font.custom("Gazpacho", size: 36).weight(.black))
-                                .foregroundColor(.black)
-                        }
-                        .padding(10)
-                        .frame(width: 232, height: 65)
-                        .background(.white)
-                        .cornerRadius(6)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 6)
-                                .inset(by: 0.5)
-                                .stroke(.black, lineWidth: 0.5)
-                        )
-                        
-                        NavigationLink(destination: SettingsView()) {
-                            IconSettings(size: "48")
-                        }
-                        .padding(.bottom, 20)
+                    VStack(spacing: 10) {
+                        Text("Create New Project")
+                            .font(Font.custom("Gazpacho", size: 36).weight(.black))
+                            .foregroundColor(.black)
                     }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding(10)
+                    .frame(width: 365, height: 65)
+                    .background(.white)
+                    .cornerRadius(6)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 6)
+                            .inset(by: 0.5)
+                            .stroke(.black, lineWidth: 0.5)
+                    )
+                    
+                    VStack(spacing: 10) {
+                        Text("My projects")
+                            .font(Font.custom("Gazpacho", size: 36).weight(.black))
+                            .foregroundColor(.black)
+                    }
+                    .padding(10)
+                    .frame(width: 232, height: 65)
+                    .background(.white)
+                    .cornerRadius(6)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 6)
+                            .inset(by: 0.5)
+                            .stroke(.black, lineWidth: 0.5)
+                    )
+                    
+                    NavigationLink(destination: SettingsView()) {
+                        IconSettings(size: "48")
+                    }
+                    .padding(.bottom, 20)
                 }
-                .frame(width: geometry.size.width, height: geometry.size.height)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
             .navigationBarHidden(true)
         }
--- melokey/SettingsView.swift
+++ melokey/SettingsView.swift
@@ -5,97 +5,71 @@ struct SettingsView: View {
     @State private var metronomeVolume: Float = 50.0
     @State private var soundFXVolume: Float = 50.0
     @State private var isDarkModeOn: Bool = false
-    
+
     var body: some View {
-        GeometryReader { geometry in
-            NavigationView {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(Color.gray.opacity(0.2))
-                        .frame(width: geometry.size.width * 0.5, height: geometry.size.height * 0.75)
-                        .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
-                    
-                    VStack(spacing: 75) {
-                        HStack {
-                            ZStack {
-                                HStack {
-                                    Button(action: {
-                                        // action for back button
-                                    }) {
-                                        Image("Union")
-                                            .font(.system(size: 24))
-                                            .foregroundColor(.black)
-                                    }
-                                    Spacer()
-                                }
-                                .padding(.leading, 50)
-                                .padding(.top, -200)
-                                
-                                Text("settings.")
-                                    .font(Font.custom("Gazpacho", size: 64).weight(.black))
-                                    .foregroundColor(.black)
-                            }
-                            .frame(maxWidth: .infinity)
+        NavigationView {
+            ZStack {
+                Color.clear.edgesIgnoringSafeArea(.all)
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.gray.opacity(0.2))
+                    .frame(width: 500, height: 400) // Fixed size for consistent layout
+                
+                VStack(spacing: 75) {
+                    HStack {
+                        NavigationLink(destination: ContentView()) {
+                            Image("Union")
+                                .font(.system(size: 24))
+                                .foregroundColor(.black)
                         }
-                        
-                        VStack(spacing: 40) {
-                            HStack {
-                                Text("Master Volume")
-                                    .frame(width: 230, alignment: .leading)
-                                Slider(value: $masterVolume, in: 0...100, step: 1)
-                                    .accentColor(.gray)
-                                    .frame(width: 200)
-                                
-                                Text("\(Int(masterVolume))")
-                                    .font(Font.custom("Gazpacho", size: 20).weight(.black))
-                                    .foregroundColor(.black)
-                                    .frame(width: 50, alignment: .leading)
-                            }
-                            
-                            HStack {
-                                Text("Metronome Volume")
-                                    .frame(width: 230, alignment: .leading)
-                                Slider(value: $metronomeVolume, in: 0...100, step: 1)
-                                    .accentColor(.gray)
-                                    .frame(width: 200)
-                                
-                                Text("\(Int(metronomeVolume))")
-                                    .font(Font.custom("Gazpacho", size: 20).weight(.black))
-                                    .foregroundColor(.black)
-                                    .frame(width: 50, alignment: .leading)
-                            }
-                            
-                            HStack {
-                                Text("Sound FX Volume")
-                                    .frame(width: 230, alignment: .leading)
-                                Slider(value: $soundFXVolume, in: 0...100, step: 1)
-                                    .accentColor(.gray)
-                                    .frame(width: 200)
-                                
-                                Text("\(Int(soundFXVolume))")
-                                    .font(Font.custom("Gazpacho", size: 20).weight(.black))
-                                    .foregroundColor(.black)
-                                    .frame(width: 50, alignment: .leading)
-                            }
-                            
-                            HStack {
-                                Text("Dark Mode")
-                                    .frame(width: 230, alignment: .leading)
-                                Toggle("", isOn: $isDarkModeOn)
-                                    .labelsHidden()
-                            }
-                        }
-                        .font(Font.custom("Gazpacho", size: 20).weight(.black))
-                        .foregroundColor(.black)
-                        
+                        Spacer()
+                        Text("settings.")
+                            .font(Font.custom("Gazpacho", size: 64).weight(.black))
+                            .foregroundColor(.black)
                     }
+                    .padding(.horizontal, 50)
+                    
+                    VStack(spacing: 40) {
+                        VolumeControl(label: "Master Volume", volume: $masterVolume)
+                        VolumeControl(label: "Metronome Volume", volume: $metronomeVolume)
+                        VolumeControl(label: "Sound FX Volume", volume: $soundFXVolume)
+                        
+                        HStack {
+                            Text("Dark Mode")
+                                .frame(width: 230, alignment: .leading)
+                            Toggle("", isOn: $isDarkModeOn)
+                                .labelsHidden()
+                        }
+                    }
+                    .font(Font.custom("Gazpacho", size: 20).weight(.black))
+                    .foregroundColor(.black)
                     .padding()
-                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 4, y: 4)
+                    .shadow(color: Color.black.opacity(0.25), radius: 4, y: 4)
                 }
-                .frame(width: geometry.size.width, height: geometry.size.height)
             }
+            .navigationBarHidden(true)
         }
-        .navigationViewStyle(StackNavigationViewStyle())    }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct VolumeControl: View {
+    var label: String
+    @Binding var volume: Float
+
+    var body: some View {
+        HStack {
+            Text(label)
+                .frame(width: 230, alignment: .leading)
+            Slider(value: $volume, in: 0...100, step: 1)
+                .accentColor(.gray)
+                .frame(width: 200)
+            Text("\(Int(volume))")
+                .font(Font.custom("Gazpacho", size: 20).weight(.black))
+                .foregroundColor(.black)
+                .frame(width: 50, alignment: .leading)
+        }
+    }
 }
 
 struct SettingsView_Previews: PreviewProvider {
