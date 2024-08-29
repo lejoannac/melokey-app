f50bef7b6e5fff0c3c08fa0d865081d79e5524f0
lejoannac <jxcai534@gmail.com>
2024-08-29 12:05:12 -0700

    starting to work on the myprojectview page

---
+++ melokey/MyProjectView.swift
@@ -0,0 +1,18 @@
+//
+//  MyProjectView.swift
+//  melokey
+//
+//  Created by joanna on 2024-08-29.
+//
+
+import SwiftUI
+
+struct MyProjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyProjectView()
+}
