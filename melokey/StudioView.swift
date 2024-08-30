8eb01d74a7c671a8a6aef2278ae45227e6d2c897
lejoannac <jxcai534@gmail.com>
2024-08-30 14:28:15 -0700

    progress

---
+++ melokey/StudioView.swift
@@ -0,0 +1,18 @@
+//
+//  StudioView.swift
+//  melokey
+//
+//  Created by joanna on 2024-08-30.
+//
+
+import SwiftUI
+
+struct StudioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudioView()
+}
