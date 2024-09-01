b277c325443efb795d5784e53091c0cc28e81586
lejoannac <jxcai534@gmail.com>
2024-08-31 20:32:42 -0700

    progress, added our library view

---
+++ melokey/OurLibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  OurLibraryView.swift
+//  melokey
+//
+//  Created by joanna on 2024-08-31.
+//
+
+import SwiftUI
+
+struct OurLibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OurLibraryView()
+}
